68adfc2a2068c69479fa7ad86044c2e2d59b69e9
Amalia Ibarra <amalia.ibarra@estudiantes.matcom.uh.cu>
2021-01-29 18:20:07 -0500

    tratando de hacerlo instalable 6

--- js/app.js
+++ js/app.js
@@ -8,6 +8,13 @@ if (navigator.serviceWorker) {
     navigator.serviceWorker.register(swLocation);
 }
 
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container
+    divInstall.classList.toggle('hidden', false);
+});
 // Referencias de jQuery
 
 var titulo = $('#titulo');
